5594ef35cbeffd4907850e7bf70d6fdd3d16d270
Mary Camacho <mary@joyasolutions.com>
2016-06-01 14:49:07 -0600

    fixed newItem api to recognize incoming object from react-form action createItem

--- server/api/new-item.js
+++ server/api/new-item.js
@@ -4,14 +4,17 @@
 
 var assert = require('assert');
 
-//add new item to closet
+//add new item to
 
 module.exports = function (dbConnection) {
     return function (req, res) {
 
-        var newItem = req.body.newItem;
-        newItem.user = req.session.userId;
+        var newItem = req.body;
+        
         newItem.createDate = new Date;
+       
+        newItem.user = req.session.userId;
+       
         dbConnection.collection('items').insertOne(newItem, function (err, result) {
             if (err) {
                 res.send("error: something bad happened and your item was not saved")
