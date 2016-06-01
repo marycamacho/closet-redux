3dc3e97cd51c0f6df0a9b7ab83455ac6e973b286
Mary Camacho <mary@joyasolutions.com>
2016-06-01 14:54:45 -0600

    initial work on item detail

--- src/components/item-details.js
+++ src/components/item-details.js
@@ -1,21 +1,21 @@
 /**
  * Created by mary on 5/29/16.
  */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchItem, deleteItem } from '../actions/index';
-import { Link } from 'react-router';
+
+
 
 class ItemDetail extends Component {
 
-
-    render() {
+    componentDidMount() {
+        const id = this.props.prams.id;
+    }
+    render () {
         return (
-            <div>
-                <h3>New Item</h3>
-                <ul className="list-group">
-                    Testing Item Detail
-                </ul>
+            <div className="selected-item">
+                Test Item Detail
             </div>
         );
     }
