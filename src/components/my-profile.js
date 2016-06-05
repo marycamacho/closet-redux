d761bd404bf6806026ddaca8b0bb0cddee474b83
Mary Camacho <mary@joyasolutions.com>
2016-06-04 20:35:03 -0600

    added framework for my profile component

--- src/components/my-profile.js
+++ src/components/my-profile.js
@@ -11,11 +11,60 @@ class MyProfile extends Component {
 
     render() {
         return (
-            <div>
-                <h3>My Profile</h3>
-                <ul className="list-group">
-                    Testing My Profile
-                </ul>
+            <div className="panel panel-default well well-white-bg col-md-12">
+                <div className="panel-heading">
+                    <h3 class-name="panel-title"><strong>My Profile</strong></h3>
+                </div>
+                <div className="panel-body required-panel">
+                    <form className="form-horizontal">
+                        <div className="col-md-4">
+                            <div className="well well-white-bg">
+                                <h3 className="panel-title">Account Details</h3>
+
+                                <div className="form-group">
+                                    <label>First Name</label>
+                                    <input className="readOnly">Mary</input>
+                                </div>
+                                <div className="form-group">
+                                    <label>Last Name</label>
+                                    <input className="readOnly">Camacho</input>
+                                </div>
+                                <div className="form-group">
+                                    <label>Email</label>
+                                    <input className="readOnly">mary@marycamacho.com</input>
+                                </div>
+                                <div className="form-group">
+                                    <label>City </label>
+                                    <input className="readOnly">Denver</input>
+                                </div>
+
+
+                            </div>
+                            <div className="well well-white-bg">
+                                <h3 className="panel-title">Style Profile</h3>
+
+                                <div className="form-group">
+                                    <label>Favorite Style</label>
+                                    <input className="readOnly">Funky Chic</input>
+                                </div>
+                                <div className="form-group">
+                                    <label>Height</label>
+                                    <input className="readOnly">5' 8"</input>
+                                </div>
+                                <div className="form-group">
+                                    <label>Weight</label>
+                                    <input className="readOnly">148</input>
+                                </div>
+                                <div className="form-group">
+                                    <label>Favorite Color</label>
+                                    <input className="readOnly">Purple</input>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
