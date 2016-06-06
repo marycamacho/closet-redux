b3d3412452ffc3fda4a8c6d34011dfd3d61d457f
Mary Camacho <mary@joyasolutions.com>
2016-06-05 18:44:49 -0600

    additional profile display

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import ItemsReducers from './reducer_items';
 import CurrentUserReducer from './reducer_current_user';
 import { reducer as formReducer } from 'redux-form';
+import { MyProfileReducer } from './reducer_profile';
 
 const rootReducer = combineReducers({
   myItems: ItemsReducers,
   form: formReducer,
-  currentUser: CurrentUserReducer
+  currentUser: CurrentUserReducer,
+  myProfile: MyProfileReducer
 });
 
 export default rootReducer;
