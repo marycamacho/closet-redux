159f0653c238068724d227b0149bfe987de8ee06
Mary Camacho <mary@joyasolutions.com>
2016-06-05 18:45:20 -0600

    cleanup

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,9 @@ import NavMenu from './nav-menu';
 export default class App extends Component {
   render() {
     return (
-      <div >
+      <div className="" >
         <NavMenu />
-        <div className="container">{this.props.children}</div>
+        <div className=" container">{this.props.children}</div>
       </div>
     );
   }
