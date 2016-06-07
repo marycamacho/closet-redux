dcfa047d95607f1a28c8b012e950998110e3adce
Mary Camacho <maryfcamacho@gmail.com>
2016-06-07 17:22:28 -0600

    item detail updated with function for differentiated button views

--- src/components/item-detail.js
+++ src/components/item-detail.js
@@ -9,6 +9,7 @@ import { Link } from 'react-router';
 
 
 
+
 class ItemDetail extends Component {
 
     static contextTypes = {
@@ -27,55 +28,85 @@ class ItemDetail extends Component {
         });
     }
 
+    // this function should return differing buttons based on the data in the item
+
+    renderButtons() {
+        const item = this.props.item;
+        if(item.isMyItem == true) {
+            return (
+                <div className="btn btn-default"
+                     onClick={this.onDeleteClick.bind(this)}>
+                    Delete Item
+                </div>
+            )
+        } else if (item.isSharedItem == true) {
+            return (
+                <div className="btn btn-default"
+                     onClick={this.onDeleteClick.bind(this)}>
+                    Remove from Shared Closet
+                </div>
+            )
+        } else {
+            return (
+                <div className="btn btn-default"
+                     onClick={this.onDeleteClick.bind(this)}>
+                    Add to Shared Closet
+                </div>
+            )
+
+        }
+    }
+
     render () {
         const {item} = this.props;
-        console.log(this.props.item);
+
+
         if(!this.props.item) {
             return <div>Loading...</div>
-        }
-        return (
-            <div className="panel panel-default well well-white-bg col-md-12 ">
-                <div className="panel-heading">
-                    <div className="col-sm-6">
-                        <h3 class-name="panel-title "><strong>Item Detail</strong></h3>
+        } else {
+            console.log(item.isMyItem);
+            return (
+                <div className="panel panel-default well well-white-bg col-md-12 ">
+                    <div className="panel-heading">
+                        <div className="col-sm-6">
+                            <h3 className="panel-title "><strong>Item Detail</strong></h3>
+                        </div>
+                        <div className=" col-sm-6">
+                            <Link to={"/"} className="top20 pull-right">
+                                <span className="btn btn-default ">Return to Closet</span>
+                            </Link>
+                        </div>
                     </div>
-                    <div className=" col-sm-6">
-                        <Link to={"/"} className="top20 pull-right">
-                            <span className="btn btn-default ">Return to Closet</span>
-                        </Link>
-                    </div>
-                </div>
-                <div className="panel-body required-panel">
+                    <div className="panel-body required-panel">
 
-                    <div className="well well-white-bg">
+                        <div className="well well-white-bg">
 
-                        <h3 className="panel-title">{item.name}</h3>
+                            <h3 className="panel-title">{item.name}</h3>
 
-                        <div className=" row my-row selected-item">
-                           <div className="col-sm-5 image-col">
-                                <img className="img-fluid detail-image" src={item.image} />
-                                <p> </p>
-                           </div>
-                           <div className="col-sm-7 ">
-                                <div className="content">
-                                    <p>Category: {item.category}</p>
-                                    <p>Style: {item.style}</p>
-                                    <p>Size: {item.size}</p>
-                                    <p>Main Color: {item.color}</p>
-                                    <p>Description: {item.description}</p>
-                                    <br /><br />
-                                    <div className="btn btn-default"
-                                        onClick={this.onDeleteClick.bind(this)}>
-                                        Delete Item
-                                    </div>
+                            <div className=" row my-row selected-item">
+                                <div className="col-sm-5 image-col">
+                                    <img className="img-fluid detail-image" src={item.image}/>
+                                    <p> </p>
                                 </div>
-                                <div className="background"></div>
-                           </div>
+                                <div className="col-sm-7 ">
+                                    <div className="content">
+                                        <p>Category: {item.category}</p>
+                                        <p>Style: {item.style}</p>
+                                        <p>Size: {item.size}</p>
+                                        <p>Main Color: {item.color}</p>
+                                        <p>Description: {item.description}</p>
+                                        <br /><br />
+                                        {this.renderButtons()}
+
+                                    </div>
+                                    <div className="background"></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            )
+        }
     }
 }
 
